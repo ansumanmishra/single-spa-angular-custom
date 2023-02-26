49ee0f008f1ed9bf1af76317d88140c6e1abeaba
Ansuman Mishra <amishra@clickbuild.de>
2023-02-26 18:47:57 +0100

    Creating main.ts and tsconfig files

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,11 +1,18 @@
+import { join, normalize } from '@angular-devkit/core';
 import {
   chain,
+  externalSchematic,
   Rule,
   SchematicContext,
   Tree,
 } from '@angular-devkit/schematics';
 
-export function singleSpaAngularCustom(options: any): Rule {
+interface SchemaOptions {
+  project: string;
+  port: number;
+}
+
+export function singleSpaAngularCustom(options: SchemaOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const workspaceConfigBuffer = tree.read('angular.json');
     if (!workspaceConfigBuffer) {
@@ -24,7 +31,11 @@ export function singleSpaAngularCustom(options: any): Rule {
 
     return chain([
       _createLocalBuildProfile(project, options, workspaceConfig),
-      _createLocalServeProfile(project, options, workspaceConfig),
+      _createLocalServeProfile(project, options, workspaceConfig, projectName),
+      _createMainTSFile(project),
+      _createTSConfigFile(project),
+      _runSingleSpaAngularSchematic(options),
+      _addPackageJsonScripts(projectName),
     ]);
   };
 }
@@ -32,8 +43,9 @@ export function singleSpaAngularCustom(options: any): Rule {
 function _createLocalServeProfile(
   project: any,
   options: any,
-  workspaceConfig: any
-) {
+  workspaceConfig: any,
+  projectName: string
+): Rule {
   return (host: Tree) => {
     const serveConfig = project.architect?.serve;
     if (!serveConfig) {
@@ -43,10 +55,8 @@ function _createLocalServeProfile(
     }
 
     const serveLocalConfig = JSON.parse(JSON.stringify(serveConfig));
-    serveLocalConfig.configurations.production.browserTarget =
-      'single-spa-angular-am-testing:build-local:production';
-    serveLocalConfig.configurations.development.browserTarget =
-      'single-spa-angular-am-testing:build-local:development';
+    serveLocalConfig.configurations.production.browserTarget = `${projectName}:production`;
+    serveLocalConfig.configurations.development.browserTarget = `${projectName}:development`;
 
     const newTargetName = 'serve-local';
     project.architect[newTargetName] = serveLocalConfig;
@@ -61,7 +71,7 @@ function _createLocalBuildProfile(
   project: any,
   options: any,
   workspaceConfig: any
-) {
+): Rule {
   return (host: Tree) => {
     const buildConfig = project.architect?.build;
     if (!buildConfig) {
@@ -79,5 +89,68 @@ function _createLocalBuildProfile(
     host.overwrite('angular.json', JSON.stringify(workspaceConfig, null, 2));
 
     return host;
+  };
+}
+
+function _createMainTSFile(project: any): Rule {
+  return (host: Tree) => {
+    const appRoot = join(normalize(project.root), 'src');
+    const mainFilePath = join(appRoot, 'main.ts');
+    const localMainFilePath = join(appRoot, 'main.local.ts');
+    const mainFileBuffer = host.read(mainFilePath);
+
+    if (!mainFileBuffer) {
+      throw new Error(`Could not find ${mainFilePath}`);
+    }
+
+    const localMainFileContent = mainFileBuffer.toString();
+
+    host.create(localMainFilePath, localMainFileContent);
+  };
+}
+
+function _createTSConfigFile(project: any): Rule {
+  return (host: Tree) => {
+    const appRoot = join(normalize(project.root), '');
+    const tsconfigFilePath = join(appRoot, 'tsconfig.app.json');
+    const tsconfigLocalFilePath = join(appRoot, 'tsconfig.app.local.json');
+    const tsconfigFileBuffer = host.read(tsconfigFilePath);
+
+    if (!tsconfigFileBuffer) {
+      throw new Error(`Could not find ${tsconfigFilePath}`);
+    }
+
+    const tsconfigLocalFileContent = tsconfigFileBuffer.toString();
+
+    host.create(tsconfigLocalFilePath, tsconfigLocalFileContent);
+  };
+}
+
+function _runSingleSpaAngularSchematic(options: SchemaOptions): Rule {
+  return async (_: Tree) => {
+    return externalSchematic('single-spa-angular', 'ng-add', options);
+  };
+}
+
+function _addPackageJsonScripts(projectName: string): Rule {
+  return (tree: Tree) => {
+    const packageJsonPath = join(normalize('package.json'));
+    const packageJsonBuffer = tree.read(packageJsonPath);
+
+    if (!packageJsonBuffer) {
+      throw new Error(`Could not find ${packageJsonPath}`);
+    }
+
+    const packageJson = JSON.parse(packageJsonBuffer.toString());
+    if (!packageJson.scripts) {
+      packageJson.scripts = {};
+    }
+
+    packageJson.scripts.start = `ng run ${projectName}:serve-local --port 4300`;
+    packageJson.scripts.build = `ng run ${projectName}:build-local`;
+
+    const updatedPackageJson = JSON.stringify(packageJson, null, 2);
+
+    tree.overwrite(packageJsonPath, updatedPackageJson);
   };
 }
